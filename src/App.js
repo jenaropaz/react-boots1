fafec3875a5f7f976e924f0b498db76a4c744424
Jenaro C. Paz <jenarocpaz@hotmail.com>
2021-02-20 19:02:43 -0700

    This files changed today

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ import Navig from './components/Navig';
 import './App.css';
 
 export default class App extends Component {
-  render() {
+  // This class has just one method -- render() -- 
+  // It renders a <div> with  className='App'
+  // This <div> has a <header>, a <Navbar> and a <div> supplied by the Router.
+
+  render() 
+  {
     return (
       <div className='App'>
         <header className='App-header'>
@@ -26,58 +31,15 @@ export default class App extends Component {
             rel='noopener noreferrer'
           >
             Learn React
-          </a></header>
-          <Navig/>
-          {/*
-          <nav className='navbar navbar-dark navbar-expand-md portfolio-navbar  gradient'>
-            <div className='container'>
-              <a className='navbar-brand' href='/'>
-                <i className='fa fa-star'></i>&nbsp;JCP
-              </a>
-              <button
-                data-toggle='collapse'
-                className='navbar-toggler'
-                data-target='#navbarNav'
-                aria-expanded="true"
-              >
-                <span className='sr-only'>Toggle navigation</span>
-                <span className='navbar-toggler-icon'></span>
-              </button>
-              <div id='navbarNav' className='collapse navbar-collapse flex-grow-0 content-justify-end' >
-                <ul className='navbar-nav ml-auto'>
-                  <li className='nav-item'>
-                    <a className='nav-link' href='/'>
-                      <i className='fa fa-home'></i>&nbsp;Home
-                    </a>
-                  </li>
-                  <li className='nav-item'>
-                    <a className='nav-link active' href='/about'>
-                      <i className='fa fa-male'></i>&nbsp;About
-                    </a>
-                  </li>
-                  <li className='nav-item'>
-                    <a className='nav-link' href='/login'>
-                      <i className='fa fa-heart'></i>&nbsp;Login
-                    </a>
-                  </li>
-                  <li className='nav-item'>
-                    <a className='nav-link' href='/contact'>
-                      <i className='fa fa-keyboard-o'></i>&nbsp;Contact
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        */}
-
+          </a>
+        </header>
+        <Navig />
         <Router>
           <Switch>
             <Route exact path='/' component={Home} />
             <Route path='/about' component={About} />
             <Route path='/contact' component={Contact} />
             <Route path='/login' component={Login} />
-
             <Route component={NoMatch} />
           </Switch>
         </Router>
